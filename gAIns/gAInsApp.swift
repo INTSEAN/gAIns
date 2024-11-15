cb9a01635a7d9ea4a3414376ed79ffe2a4441d98
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2024-11-14 22:50:25 -0600

    Initial Commit

---
+++ gAIns/gAInsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gAInsApp.swift
+//  gAIns
+//
+//  Created by SEAN DONOVAN on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct gAInsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gAInsTests/gAInsTests.swift
@@ -0,0 +1,17 @@
+//
+//  gAInsTests.swift
+//  gAInsTests
+//
+//  Created by SEAN DONOVAN on 11/14/24.
+//
+
+import Testing
+@testable import gAIns
+
+struct gAInsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
