0540749c4371d65970dc4adbfdb24a83b6d2a6b1
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2024-11-15 00:27:30 -0600

    Added Spotify, AI and HomeView

---
+++ gAIns/Views/SpotifyPage.swift
@@ -0,0 +1,47 @@
+//
+//  SpotifyPage.swift
+//  gAIns
+//
+//  Created by SEAN DONOVAN on 11/14/24.
+//
+import SwiftUI
+
+struct SpotifyPage: View {
+    @State private var isMainPagePresented = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("landing_background_image") // Replace with your landing page background image
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(maxHeight: .infinity)
+
+                Spacer()
+
+                Button(action: {
+                    isMainPagePresented = true
+                }) {
+                    Text("Start Here")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black) // Customize the button color
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            .navigationBarHidden(true)
+            .fullScreenCover(isPresented: $isMainPagePresented, content: {
+                SpotifyView()
+            })
+        }
+    }
+}
+
+struct LandingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotifyPage()
+    }
+}
